cc6d6493a6cfd12811f7fa491acd031d68c91252
facutomasetti <ftomasetti@frba.utn.edu.ar>
2025-09-19 23:36:27 -0300

    antes del merge

--- packages/backend/domain/notificacion/FactoryNotificacion.js
+++ packages/backend/domain/notificacion/FactoryNotificacion.js
@@ -1,3 +1,5 @@
+import { Notificacion } from "./Notificacion.js";
+
 export class FactoryNotificacion {
   static crearSegunEstadoPedido(estadoPedido) {
     return `El pedido pasó al estado: ${estadoPedido}`;
